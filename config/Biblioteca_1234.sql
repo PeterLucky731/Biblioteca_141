f59ae97921af4f361fe398f81197e14e43269eac
Pedro Lucas Correia Martin <pedrolucasrbsg@gmail.com>
2024-11-01 07:53:49 -0400

    commit work 01-11-24

---
+++ config/Biblioteca_1234.sql
@@ -0,0 +1,41 @@
+create database biblioteca_1234;
+use biblioteca_1234;
+
+create table usuario(
+	id int primary key auto_increment,
+	nome varchar(80) not null,
+    email varchar(80) not null,
+    senha varchar(16),
+    data_nasc date
+);
+
+create table livro(
+	id int primary key auto_increment,
+	titulo varchar(50) not null,
+    autor varchar(80) not null,
+    genero varchar(30),
+    descricao text(200)
+);
+
+create table emprestimo(
+	id int primary key auto_increment,
+    data_emprestimo date,
+    id_usuario int,
+    id_livro int,
+	foreign key (id_usuario) references usuario(id),
+    foreign key (id_livro) references livro(id)   
+);
+
+create table devolucao(
+	id int primary key auto_increment,
+    data_devolucao date,
+    id_emprestimo int,
+    id_usuario int,
+    id_livro int,
+	foreign key (id_usuario) references usuario(id),
+    foreign key (id_livro) references livro(id),
+    foreign key (id_emprestimo) references emprestimo(id)
+);
+
+insert into livro(titulo, autor, genero, descicao) values ("","","","");
+
