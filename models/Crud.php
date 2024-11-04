5950fe5edee62f8d9db3bb1d3097a9bb8b1f0aef
Ninjjer <henrique.gianottiv@gmail.com>
2024-11-04 10:08:29 -0400

    Push para outros darem pull

--- models/Crud.php
+++ models/Crud.php
@@ -3,4 +3,7 @@
 interface Crud
 {
     public function cadastrar();
+    public function ler();
+    public function alterar();
+    public function deletar();
 }
--- pages/Cadastro_Livro.php
+++ pages/Cadastro_Livro.php
@@ -4,7 +4,8 @@ require_once 'controllers/LivroController.php';
 
 $acao = isset($_GET['acao']) ? $_GET['acao'] : '';
 
-switch($acao){
+switch($acao)
+{
     case 'cadastrar':
         $livroController = new LivroController();
         $livroController->cadastrarLivro($_POST['titulo'], $_POST['autor'], $_POST['genero']);
