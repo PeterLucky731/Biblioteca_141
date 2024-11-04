9099c2486a430fed4e574d412874401df6328134
Pedro Lucas Correia Martin <pedrolucasrbsg@gmail.com>
2024-11-04 07:26:15 -0400

    alterações

--- views/formCadastrarLivro.php
+++ views/formCadastrarLivro.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./views/style.css">
     <title>Document</title>
 </head>
+
 <body>
     <section id="sec-form">
     <form id="box-form" action="index.php?acao=cadastrar" method="post">
