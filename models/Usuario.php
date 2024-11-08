466e2c7f6f427523adb2caf4c22928decc24cf6c
Ninjjer <henrique.gianottiv@gmail.com>
2024-11-08 10:54:17 -0400

    Adicionando o models usuario.php para o Everton mexer depois

--- models/Usuario.php
+++ models/Usuario.php
@@ -1,8 +1,8 @@
 <?php
 
-require "./models/Crud.php";
+require_once 'Crud.php';
 
-class Usuario implements Crud
+class Usuario
 {
     private $conexao;
     private $table = "usuario";
@@ -30,8 +30,45 @@ class Usuario implements Crud
         $query = "INSERT INTO {$this->table} (nome, email, senha, data_nasc) values ('{$this->nome}','{$this->email}','{$this->senha}','{$this->data_nasc}');";
         return $this->conexao->query($query);
     }
-    public function loginUsuario()
+
+    public function logar()
     {
-        
+        $query = "SELECT * FROM {$this->table} WHERE email = ? LIMIT 1";
+        $stmt = $this->conexao->prepare($query);
+        $stmt->bind_param("s", $this->email);
+        $stmt->execute();
+        $resultado = $stmt->get_result();
+
+        if ($resultado->num_rows > 0)
+        {
+            $usuario = $resultado->fetch_assoc();
+            if (password_verify($this->senha, $usuario['senha'])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function __destruct() {
+        if ($this->conexao) {
+            $this->conexao->close();
+        }
+    }
+
+    public function ler()
+    {
+        $query = "SELECT * FROM {$this->table}";
+        return $this->conexao->query($query);
+    }
+
+    public function atualizar()
+    {
+        $query = "UPDATE {$this->table} SET nome = {$this->nome}, email = {$this->email}, senha = {$this->senha}, data_nasc = {$this->data_nasc} WHERE id = {$this->id}";
+    }
+
+    public function deletar()
+    {
+        $query = "DELETE FROM {$this->table} WHERE id = {$this->id}";
+        return $this->conexao->prepare($query);
     }
 }
