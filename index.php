9e7cbbfa943f084b68b87fc6cc8830b5c5c137fb
Pedro Lucas Correia Martin <pedrolucasrbsg@gmail.com>
2024-11-11 08:58:30 -0400

    Hasagiiigit add .git add .

--- index.php
+++ index.php
@@ -1,25 +1,17 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-</head>
-<body>
-    <!-- tem que mudar isso daqui depois -->
-    <h1>Menu de Bosta</h1>
-    <ul>
-        <li><a href="/Biblioteca_141/pages/Login.php">Fazer Login</a></li>
-        <li><a href="/Biblioteca_141/pages/Cadastro_Livro.php">Cadastrar Livro</a></li>
-        <li><a href="/Biblioteca_141/pages/Cadastro_Usuario.php">Cadastrar Usuário</a></li>
-        <li><a href="/Biblioteca_141/pages/Emprestimo_Livro.php">Registrar Empréstimo</a></li>
-        <li><a href="/Biblioteca_141/pages/Consultar_LIvro.php">Consultar Livro</a></li>
-    </ul>
+<?php
 
-        <!-- isso daqui é balela minha -->
-    <h1 style="font-size: 100px;">BEM-VINDO A BIBLIOTECA DO SHADOW (work in progress)</h1>
+require_once './controllers/LoginController.php';
 
-    <img src="https://media.tenor.com/On7kvXhzml4AAAAj/loading-gif.gif" alt="wip" style="width: 300px; height: auto;">
+$acao = isset($_GET['acao']) ? $_GET['acao'] : '';
+
+switch($acao)
+{
+    case 'login':
+        $loginController = new loginController();
+        $loginController->login($_POST['email'], $_POST['senha']);
+        break;
     
-</body>
-</html>
+    default:
+        include 'views/formLogin.php';
+}
+
